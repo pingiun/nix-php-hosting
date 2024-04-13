4edf818cecd6b9e1d3d594931f199f9722999f8c
Jelle Besseling <jelle@pingiun.com>
2024-04-13 16:51:35 +0200

    Set jdk versions for opensearch

--- packages/opensearches/opensearch/1.2.nix
+++ packages/opensearches/opensearch/1.2.nix
@@ -1,8 +1,9 @@
-{ callPackage }:
+{ callPackage, jre_headless }:
 callPackage ./generic.nix {
   version = "1.2.4";
   hashes = {
     "x86_64-linux" = "1a6q9dsik8lj68bvmm0md0i1lrkcga2f55sr4fm6crrvcab2c3yl";
     "aarch64-linux" = "19b37sfzf67blkp004cqa27cwhvcq42lacsld8l4l7l3s4xd332y";
   };
+  inherit jre_headless;
 }
--- packages/opensearches/opensearches.nix
+++ packages/opensearches/opensearches.nix
@@ -2,8 +2,14 @@ nixpkgs:
 final:
 prev:
 {
-  opensearch_12 = prev.callPackage ./opensearch/1.2.nix {};
-  opensearch_13 = prev.callPackage ./opensearch/1.3.nix {};
-  opensearch_25 = prev.callPackage ./opensearch/2.5.nix {};
+  opensearch_12 = prev.callPackage ./opensearch/1.2.nix {
+    jre_headless = prev.jdk11_headless;
+  };
+  opensearch_13 = prev.callPackage ./opensearch/1.3.nix {
+    jre_headless = prev.jdk11_headless;
+  };
+  opensearch_25 = prev.callPackage ./opensearch/2.5.nix {
+    jre_headless = prev.jdk17_headless;
+  };
   opensearch_212 = prev.callPackage ./opensearch/2.12.nix {};
 }
