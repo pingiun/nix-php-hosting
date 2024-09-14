5ebfef7ebb7cd851196276c360fb30f6d35a0fea
Jelle Besseling <jelle@pingiun.com>
2024-09-14 09:02:26 +0200

    Fix rabbitmq 3.11

--- packages/rabbitmqs/rabbitmq/3.11.nix
+++ packages/rabbitmqs/rabbitmq/3.11.nix
@@ -2,9 +2,10 @@
 , AppKit
 , Carbon
 , Cocoa
+, elixir
 }:
 callPackage ./generic.nix {
   version = "3.11.28";
   hash = "sha256-heOzYhtqEnIU8Tt1P5r9l3bYZS9rFGnknZgCf3X0HKo=";
-  inherit AppKit Carbon Cocoa;
+  inherit AppKit Carbon Cocoa elixir;
 }
