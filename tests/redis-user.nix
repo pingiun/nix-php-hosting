667131a0eb18fe3a61ce6d00078b927f4f8084ac
Jelle Besseling <jelle@pingiun.com>
2024-09-16 20:13:07 +0200

    Add redis user test

---
+++ tests/redis-user.nix
@@ -0,0 +1,33 @@
+projectModule:
+{ lib, ... }:
+{
+  name = "redis";
+  meta.maintainers = with lib.maintainers; [ flokli ];
+
+  nodes = {
+    machine =
+      { pkgs, lib, ... }:
+
+      {
+        imports = [ projectModule ];
+        projects.test = {
+          services.redis.servers."".enable = true;
+        };
+      };
+  };
+
+  testScript =
+    { nodes, ... }:
+    let
+      inherit (nodes.machine.services) redis;
+    in
+    ''
+      start_all()
+      machine.wait_for_unit("redis", "test")
+
+      machine.wait_for_file("${redis.servers."".unixSocket}")
+
+      # The unix socket is accessible to the redis group
+      machine.succeed('su test -c "redis-cli -s ${redis.servers."".unixSocket} ping | grep PONG"')
+    '';
+}
