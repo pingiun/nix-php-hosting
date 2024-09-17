7444ef36965a083dda482dddb1914fc97410f36c
Jelle Besseling <jelle@pingiun.com>
2024-09-17 20:04:57 +0200

    Disable broken rabbitmq version

--- packages/rabbitmqs/elixir/default.nix
+++ packages/rabbitmqs/elixir/default.nix
@@ -10,14 +10,14 @@ let
   callElixir =
     drv: args:
     let
-      builder = callPackage ../generic-builder.nix args;
+      builder = callPackage ./generic-builder.nix args;
     in
     callPackage drv {
       mkDerivation = pkgs.makeOverridable builder;
     };
 in
 {
-  elixir_1_17 = lib.callElixir ../1.17.nix {
+  elixir_1_17 = callElixir ./1.17.nix {
     inherit erlang;
     debugInfo = true;
   };
--- packages/rabbitmqs/rabbitmqs.nix
+++ packages/rabbitmqs/rabbitmqs.nix
@@ -3,7 +3,7 @@ nixpkgs:
 final: prev:
 
 let
-  elixir_1_17 = prev.callPackage ./elixir/1.17.nix { };
+  elixirs = prev.callPackage ./elixir { };
 in
 
 {
@@ -22,9 +22,9 @@ in
     inherit (prev.darwin) AppKit Carbon Cocoa;
     erlang = prev.erlang_25;
   };
-  rabbitmq_313 = prev.callPackage ./rabbitmq/3.13.nix {
-    inherit (prev.darwin) AppKit Carbon Cocoa;
-    erlang = prev.erlang_26;
-    elixir = elixir_1_17;
-  };
+  # rabbitmq_313 = prev.callPackage ./rabbitmq/3.13.nix {
+  #   inherit (prev.darwin) AppKit Carbon Cocoa;
+  #   erlang = prev.erlang_26;
+  #   elixir = elixirs.elixir_1_17;
+  # };
 }
