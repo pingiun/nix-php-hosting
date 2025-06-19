6ba7c14068a9f03fd00fb72bde6a1b37040645e9
Jelle Besseling <jelle@bigbridge.nl>
2025-06-19 12:43:02 +0200

    Elixir fixes

--- packages/rabbitmqs/elixir/default.nix
+++ packages/rabbitmqs/elixir/default.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   erlang,
+  erlang_26,
   lib,
   callPackage,
   ...
@@ -17,6 +18,10 @@ let
     };
 in
 {
+  elixir_1_15 = callElixir ./1.15.nix {
+    erlang = erlang_26;
+    debugInfo = true;
+  };
   elixir_1_17 = callElixir ./1.17.nix {
     inherit erlang;
     debugInfo = true;
--- packages/rabbitmqs/rabbitmqs.nix
+++ packages/rabbitmqs/rabbitmqs.nix
@@ -16,12 +16,13 @@ in
   # };
   rabbitmq_311 = prev.callPackage ./rabbitmq/3.11.nix {
     inherit (prev.darwin) AppKit Carbon Cocoa;
-    elixir = prev.elixir_1_15;
+    erlang = prev.erlang_26;
+    elixir = elixirs.elixir_1_15;
   };
   rabbitmq_312 = prev.callPackage ./rabbitmq/3.12.nix {
     inherit (prev.darwin) AppKit Carbon Cocoa;
-    erlang = prev.erlang_25;
-    elixir = prev.elixir_1_15;
+    erlang = prev.erlang_26;
+    elixir = elixirs.elixir_1_15;
   };
   rabbitmq_313 = prev.callPackage ./rabbitmq/3.13.nix {
     inherit (prev.darwin) AppKit Carbon Cocoa;
