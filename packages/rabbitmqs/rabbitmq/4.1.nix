f77874e2175cc18cfaac20df86ee77094b71f338
Jelle Besseling <jelle@bigbridge.nl>
2025-06-19 17:28:31 +0200

    Hashes

--- packages/rabbitmqs/rabbitmq/4.1.nix
+++ packages/rabbitmqs/rabbitmq/4.1.nix
@@ -8,7 +8,7 @@
 }:
 callPackage ./generic.nix {
   version = "4.1.1";
-  hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  hash = "sha256-eIoa8ZwnVNhV+6yQOT5LqfhgvT1TfYaPIgSyVkV5HFI=";
   inherit
     AppKit
     Carbon
