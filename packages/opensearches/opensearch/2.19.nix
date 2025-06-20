447424bde3f9928a4c3ed5b360ebca32fc868138
Jelle Besseling <jelle@bigbridge.nl>
2025-06-20 11:19:00 +0200

    Hashes

--- packages/opensearches/opensearch/2.19.nix
+++ packages/opensearches/opensearch/2.19.nix
@@ -3,6 +3,6 @@ callPackage ./generic.nix {
   version = "2.19.2";
   hashes = {
     "x86_64-linux" = "sha256-EaOx8vs3y00ln7rUiaCGoD+HhiQY4bhQAzu18VfaTYw=";
-    "aarch64-linux" = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    "aarch64-linux" = "sha256-/2sP7HRApfnFzVvcg5n28rqqkWLS+B0UzxUtSwlyErE=";
   };
 }
