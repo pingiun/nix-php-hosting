44137a7d2359e5bc9d2bf5027f33822d828702bc
Jelle Besseling <jelle@bigbridge.nl>
2025-06-25 14:20:00 +0200

    Add varnish 7.6 modules

--- packages/varnishes/varnishes.nix
+++ packages/varnishes/varnishes.nix
@@ -1,6 +1,6 @@
 nixpkgs: final: prev:
 
-{
+rec {
   varnish64 = prev.callPackage ./varnish/6.4.nix { };
   varnish65 = prev.callPackage ./varnish/6.5.nix { };
   varnish70 = prev.callPackage ./varnish/7.0.nix { };
@@ -8,4 +8,5 @@ nixpkgs: final: prev:
   varnish73 = prev.callPackage ./varnish/7.3.nix { };
   varnish75 = prev.callPackage ./varnish/7.5.nix { };
   varnish76 = prev.callPackage ./varnish/7.6.nix { };
+  varnish76Modules = (prev.callPackages ./modules.nix { varnish = varnish76; }).modules25;
 }
